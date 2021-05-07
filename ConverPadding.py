a492448db7239e215991edb7b95d4e55b6fda420
Miranda117 <a01769232@itesm.mx>
2021-05-07 01:14:11 -0500

    Se corrige el error de salida y optimiza el codigo

--- ConverPadding.py
+++ ConverPadding.py
@@ -1,65 +1,34 @@
 import numpy as np
+from scipy import signal
 
 
-def conv_helper(fragment, kernel):
-    f_row, f_col = fragment.shape
-    k_row, k_col = kernel.shape
-    result = 0.0
-    for row in range(f_row):
-        for col in range(f_col):
-            result += fragment[row, col] * kernel[row, col]
-    return result
-
-
-def convolution(image, kernel):
-
-    image_row, image_col = image.shape
-    kernel_row, kernel_col = kernel.shape
-    output = np.zeros(image.shape)
-
-    for row in range(image_row):
-        for col in range(image_col):
-                output[row, col] = conv_helper(
-                        image[row:row + kernel_row,
-                        col:col + kernel_col], kernel
-                        )
-
-    print (output)
-    return output
-
-
-def padding(image):
-    row,col=image.shape
-    image_padding = np.zeros([row+2,col+2])
-    x=0
-    y=0
-    for i in range (row):
-        for j in range (col):
-
-            image_padding[i +1, j+1] = image[i, j]
-           
-    print (image_padding)
-    return image_padding
+# Funcion para calcular la convolución de una matriz de dos dimensiones
+def convolutionPadding(image, kernel):
+    #Se hace uso de el metodo convolve2d que viene de la libreria scipy
+    output_matrix = signal.convolve2d(image, kernel, mode="same")
+    print ("this is the final matix")
+    print (output_matrix)
+    return output_matrix
 
 
 def request_matrix():
+    #El usuario ingresa las los renglones y columnas de la matriz de la imagen
     rowImage = int(input("Enter the number of rows of the image matrix: "))
     colImage = int(input("Enter the number of cols of the image matrix: "))
     image = np.zeros([rowImage, colImage])
 
     for i in range(0, rowImage, 1):
         for j in range(0, colImage, 1):
-
             valueMatri = float(input("Enter the value: "))
             image[i, j] = valueMatri
     print ("Original matrix:")
     print(image)
-    image_padding=padding(image)
     kernel = request_kernel()
-    return image_padding, kernel
+    return image, kernel
 
 
 def request_kernel():
+    #El usuario ingresa las los renglones y columnas de la matriz de la kernel
     row_kernel = int(input("Enter the number of rows of the kernel: "))
     col_kernel = row_kernel
     kernel = np.zeros([row_kernel, col_kernel])
@@ -69,10 +38,10 @@ def request_kernel():
 
             value_kernel = float(input("Enter the value: "))
             kernel[i, j] = value_kernel
-    print ("kernel:")
+    print ("kernel: ")
     print(kernel)
     return kernel
 
 if __name__ == "__main__":
     image, kernel = request_matrix()
-    convoli = convolution(image, kernel)
+    convoli = convolutionPadding(image, kernel)
